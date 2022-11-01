1b86c5d46fb312a2a90c0dc6225a6281801d8a12
Jasmeet Singh Chawla <60892009+jasi381@users.noreply.github.com>
2022-11-01 10:56:48 +0530

    Loading button done but with scale problem

---
+++ customButtons/src/main/java/com/jasmeet/custombuttons/LoadingBtn.kt
@@ -0,0 +1,23 @@
+package com.jasmeet.custombuttons
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+import com.airbnb.lottie.LottieAnimationView
+
+class LoadingBtn(context: Context, attrs:AttributeSet):LottieAnimationView(context,attrs) {
+
+    private var mContext = context
+    private var  a = mContext.obtainStyledAttributes(attrs,R.styleable.LoadingBtn)
+
+    fun loadingBtn(){
+        val lottieRes = a.resources.openRawResource(R.raw.loader)
+        val loop = a.getBoolean(R.styleable.LoadingBtn_lottie_loop, true)
+        val autoPlay = a.getBoolean(R.styleable.LoadingBtn_lottie_autoPlay,true)
+
+
+    }
+
+
+
+}
